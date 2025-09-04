3a34e5816b965e4b188093d8e0fb5ef49a712642
VishalxVG <avatarvishal1551@gmail.com>
2025-09-04 22:06:36 +0530

    Update start.sh file

--- Backend/start.sh
+++ Backend/start.sh
@@ -20,5 +20,5 @@ END
 # Run Alembic migrations
 alembic upgrade head
 
-# Start FastAPI
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000
+# Start FastAPI (bind to Render's dynamic port)
+exec uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
